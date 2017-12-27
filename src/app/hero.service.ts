c416c341120cac408923d50c1857f9e267d36a45
Alen Pelin <alenpelin@gmail.com>
2017-12-27 19:29:51 +1100

    Simulate restful server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,6 +5,8 @@ import { ArrayObservable } from '../lib/rxjs/Observable/ArrayObservable';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import {catchError, map, tap } from 'rxjs/operators';
+
 @Injectable()
 export class HeroService {
 
@@ -18,7 +20,8 @@ export class HeroService {
   get Heroes(): Observable<Hero[]>{
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(catchError(this.handleError('getHeroes', [])));
   }
 
   getHero(id: number): Observable<Hero> {
@@ -30,5 +33,25 @@ export class HeroService {
 
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return new ArrayObservable([ result as T ]);
+    };
   }
 }
