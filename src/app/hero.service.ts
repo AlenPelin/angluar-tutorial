6b454e7f3992b89960a0718c93d4b5ebb69f527b
Alen Pelin <alenpelin@gmail.com>
2017-12-27 18:52:16 +1100

    Add dashboard and router

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,13 +3,18 @@ import { HEROES } from '../mock-heroes';
 import { Hero } from './hero';
 import { Observable } from 'rxjs/Observable';
 import { ArrayObservable } from '../lib/rxjs/Observable/ArrayObservable';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   get Heroes(): Observable<Hero[]>{
+    // Todo: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return new ArrayObservable([ HEROES ]); 
   }
 }
